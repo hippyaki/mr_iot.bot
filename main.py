d71b3a1df913db9bb39c58253b59f23b41b6aba8
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-04-15 23:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from instabot import Bot
-import time, os.path
-import conf
- 
-directories = ["esp8266", "arduinonano" , "esp32", "iot", "iotprojects", "homeautomation", "digitalelectronics"]
+import time, os
+
+directories = ["arduinonano" , "esp8266", "esp32", "iot", "iotprojects", "homeautomation", "digitalelectronics"]
 i=1
 bot = Bot()
 
-bot.login(username = conf.username, password = conf.password)
-            
+bot.login(username = os.environ.get("USERNAME"), password = os.environ.get("PASSWORD"))
+
 try:
     for directory in directories:
         for i in range(5):
             
-            filename = f"images/{directory}/{i}.jpg"
+            filename = f"../images/{directory}/{i}.jpg"
             file = os.path.exists(filename)
             if file: 
-                bot.upload_photo(filename,caption =f"Follow : @mr_iot.bot & @school_of_iot \n.\nSource : @{directory}\n.\nTags ⏬\n____________\n#arduino #arduinoproject #arduinouno #arduinofun #embedded #digitalelectronics #arduinonano #arduinoprogramming #arduinoprojects  #arduinoproject #arduinomega #arduinolove #electronics #diyelectronics #raspberrypi4 #raspberrypi3 #raspberrypi")
+                bot.upload_photo(filename,caption =f"Follow : @mr_iot.bot & @school_of_iot \n.\nSource : Unknown\nTag scrape: #{directory}\n.\nTags ⏬\n____________\n#arduino #arduinoproject #arduinouno #arduinofun #embedded #digitalelectronics #arduinonano #arduinoprogramming #arduinoprojects  #arduinoproject #arduinomega #arduinolove #electronics #diyelectronics #raspberrypi4 #raspberrypi3 #raspberrypi", upload_id=None, from_video=False)
+                #bot.upload_photo(filename, caption = "This is a caption", upload_id=None, from_video=False)
+                
                 print(f"Posted photo - {i} from {directory}")
-                time.sleep(43200)
+                print("")
+                time.sleep(3600) # 60 mins
             i = i + 1
+            
             
 except Exception as e:
     print("Error:")
